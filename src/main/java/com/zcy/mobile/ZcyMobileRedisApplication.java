d9bc72d1629ebcea9c3cb2438e73913e831a2969
dazhong1102 <523463619@qq.com>
2021-09-01 10:20:56 +0800

    "redis提交测试"

---
+++ src/main/java/com/zcy/mobile/ZcyMobileRedisApplication.java
@@ -0,0 +1,13 @@
+package com.zcy.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZcyMobileRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZcyMobileRedisApplication.class, args);
+    }
+
+}
